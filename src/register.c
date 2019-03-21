0cc7635b11912ead574c213fb972ea5b6e0b5552
Jeroen Ooms <jeroenooms@gmail.com>
2019-03-21 12:57:07 +0100

    visibility

--- src/register.c
+++ src/register.c
@@ -1,7 +1,19 @@
+#include <R.h>
 #include <Rinternals.h>
+#include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
+#include <R_ext/Visibility.h>
 
-void R_init_bcrypt(DllInfo* info) {
-  R_registerRoutines(info, NULL, NULL, NULL, NULL);
-  R_useDynamicSymbols(info, TRUE);
+extern SEXP R_encode_salt(SEXP, SEXP);
+extern SEXP R_hashpw(SEXP, SEXP);
+
+static const R_CallMethodDef CallEntries[] = {
+  {"R_encode_salt", (DL_FUNC) &R_encode_salt, 2},
+  {"R_hashpw",      (DL_FUNC) &R_hashpw,      2},
+  {NULL, NULL, 0}
+};
+
+attribute_visible void R_init_bcrypt(DllInfo *dll){
+  R_registerRoutines(dll, NULL, CallEntries, NULL, NULL);
+  R_useDynamicSymbols(dll, FALSE);
 }
